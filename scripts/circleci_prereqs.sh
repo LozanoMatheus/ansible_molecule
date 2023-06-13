351824ba72fe656a66460f3e5bf0268f5603aabc
Matheus Lozano <lozanomatheus@gmail.com>
2023-06-13 23:07:01 +0200

    Remove apt-key

--- scripts/circleci_prereqs.sh
+++ scripts/circleci_prereqs.sh
@@ -22,8 +22,8 @@ function install_virtualbox() {
   sudo apt-get update -y &> /dev/null
   sudo apt-get install -y linux-headers-$(uname -r) &> /dev/null
   echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian \"${LINUX_RELEASE}\" contrib" | sudo tee -a /etc/apt/sources.list
-  wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-  wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+  wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
+  wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
   sudo apt-get update -y &> /dev/null
   sudo apt-get install -y virtualbox-6.1 &> /dev/null
   log_msg "VirtualBox version is $(VBoxManage --version)"
